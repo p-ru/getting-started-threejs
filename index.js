d41d9905e292601cbc684864e035590387839bfa
Bobby Roe <bobby.roe@unifyconsulting.com>
2025-04-10 07:37:29 -0700

    add Astronaut

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import { OrbitControls } from "jsm/controls/OrbitControls.js";
 const w = window.innerWidth;
 const h = window.innerHeight;
 const scene = new THREE.Scene();
+
 const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
 camera.position.z = 5;
 const renderer = new THREE.WebGLRenderer();
